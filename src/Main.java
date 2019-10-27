4dd90276c647dd7d6abed7376ebecc539bce41c6
108021053 <108021052@live.asia.edu.tw>
2019-10-27 12:41:48 +0800

    1/[(2n-1)*(2n)]

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
-        float a=scn.nextInt();
-        float sum=1;
-        for(float i=1;i<=a;i++){
-            sum=sum*i;
+        double a=scn.nextInt();
+        double sum=0;
+        for(double i=1;i<=a;i++){
+            sum=sum+1/((2*i-1)*(2*i));
         }
         System.out.println(sum);
     }

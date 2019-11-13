e44e084ff66ee1c09346573678c9c78efd158f1a
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:29:48 +0800

    直角三角形邊長

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        BigInteger a=new BigInteger("1");
-        int  b = 0;
-        for (int i = n; i > 1; i--) {
-            a=a.multiply(new BigInteger(Integer.toString(i)));
+        int n=scn.nextInt();
+        for (int i=3;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                for (int k=j+1;k<n;k++){
+                    if((i+j+k)<=n){
+                        if((i*i+j*j)==k*k){
+                            System.out.println(i+"\t"+j+"\t"+k);
+                        }
+                    }
+                }
+            }
         }
-        while (a.mod(new BigInteger("10")).intValue()==0){
-            b++;
-            a=a.divide(new BigInteger("10"));
-        }
-        System.out.println(b);
     }
 }

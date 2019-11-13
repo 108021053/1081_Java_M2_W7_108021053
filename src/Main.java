ad082e0850a93329f990920305b255d0a2d3ffcd
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:11:29 +0800

    連續0個數

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-
+import java.math.BigInteger;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-        int i=1,j=1;
-        while (i<=n){
-            System.out.print(i+"\t");
-            i=i+j;
-            j++;
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        BigInteger a=new BigInteger("1");
+        int  b = 0;
+        for (int i = n; i > 1; i--) {
+            a=a.multiply(new BigInteger(Integer.toString(i)));
         }
-        System.out.println(i);
-        i=1;
-        while (i<=(n+1)){
-            for (int k=1;k<=i;k++){
-                System.out.print(i+"\t");
-            }
-            i++;
+        while (a.mod(new BigInteger("10")).intValue()==0){
+            b++;
+            a=a.divide(new BigInteger("10"));
         }
-        System.out.println();
+        System.out.println(b);
     }
 }

35b3a8559e090523467dfda50f7162460dc37a4d
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:40:44 +0800

    數字反轉

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n=scn.nextInt();
-        for (int i=3;i<n;i++){
-            for (int j=i+1;j<n;j++){
-                for (int k=j+1;k<n;k++){
-                    if((i+j+k)<=n){
-                        if((i*i+j*j)==k*k){
-                            System.out.println(i+"\t"+j+"\t"+k);
-                        }
-                    }
-                }
+        long n=scn.nextInt();
+        while (n>0){
+            System.out.print(n%10);
+            n/=10;
             }
+        System.out.println();
         }
     }
-}
+
